cb366e91f3425c6a248c958bc69de6c29bd415be
ChaiCaroline <chaiene_caroline@hotmail.com>
2023-07-21 20:18:20 -0300

    Primeiro commit inicializando Projeto Front End

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function Main() {
+
+  return (
+    <div className='main'>
+      <button className='initial fontStyleNunito3 butMain'><h1>Entrar Agora</h1></button>
+    </div>
+  )
+}
+
+export default Main
