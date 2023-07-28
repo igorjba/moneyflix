d784d90e47f5df07cf699ca182eb4de1ae57ff7e
Igor <igorjbahia@gmai.com>
2023-07-28 10:59:21 -0300

    :sparkles: feat: criado comando para deploy.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/igorjba/front-integral-m05-t10",
   plugins: [react()],
 })
 
